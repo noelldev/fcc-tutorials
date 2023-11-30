febf057978e1ca6b71d693b10cdc8dac83bc2fea
kodrr <ntlauigan@gmail.com>
2023-11-30 16:44:55 +0800

    Solve Boo Who

---
+++ basic-algorithm-scripting/finders-keepers.js
@@ -0,0 +1,19 @@
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'.
+//This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+
+function findElement(arr, func) {
+  return arr.find(func);
+}
+
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
+
+//Boo Who
+
+function booWho(bool) {
+  if (bool === true || bool === false) {
+    return true;
+  }
+  return false;
+}
+
+booWho(null);
